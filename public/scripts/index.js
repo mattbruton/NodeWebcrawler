f4babb07a1506ef1ab863883913f7df1184d6c2c
Matt Bruton <the.bruton@gmail.com>
2017-05-09 18:54:00 -0500

    minor tweaks to accommodate for iframe

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,1 +1,22 @@
 console.log('hi!');
+
+const fetchPage = () => {
+    return new Promise((resolve, reject) => {
+        fetch('./page')
+            .then(response => response.text())
+            .then(data => {
+                resolve(data);
+            });
+    });
+};
+
+document.getElementById('btn').addEventListener('click', () => {
+    fetchPage()
+    .then(response => {
+        var iframe = document.createElement('iframe');
+        iframe.setAttribute('srcdoc', response);
+        iframe.setAttribute('height', '400px')
+        iframe.setAttribute('width', '800px');
+        document.getElementById('container__results').appendChild(iframe);
+    });
+})
